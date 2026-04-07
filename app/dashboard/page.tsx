82bba3f26e140699fd48009471c9f9e95a6e5acf
faizm10 <faizmustansar10@gmail.com>
2026-04-06 20:56:05 -0400

    chore: white dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,3 @@
-import { Map, MapControls } from "@/components/ui/map";
-import { Card } from "@/components/ui/card";
-
-export function MyMap() {
-  return (
-    <Card className="h-[320px] p-0 overflow-hidden">
-      <Map center={[-74.006, 40.7128]} zoom={11}>
-        <MapControls />
-      </Map>
-    </Card>
-  );
+export default function Page() {
+  return <main className="min-h-[100dvh] bg-white" />
 }
